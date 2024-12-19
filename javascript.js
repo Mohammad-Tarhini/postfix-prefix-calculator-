4096e8658405a9342405bf57980ac5464bd03b70
mohamad baker tarhini <tarhinimohammadbaker@gmail.com>
2024-12-19 06:27:19 +0200

    clean the code and add some features

--- javascript.js
+++ javascript.js
@@ -1,134 +1,131 @@
-//****elements********** */
-let plus=document.getElementById("+")
-let minus=document.getElementById("-")
-let times=document.getElementById("*")
-let divide=document.getElementById("/")
-let zero=document.getElementById("0")
-let one=document.getElementById("1")
-let two=document.getElementById("2")
-let three=document.getElementById("3")
-let four=document.getElementById("4")
-let five=document.getElementById("5")
-let six=document.getElementById("6")
-let seven=document.getElementById("7")
-let eight=document.getElementById("8")
-let nine=document.getElementById("9")
-let equal=document.getElementById("=")
-let screen=document.getElementById("screen")
-let screen1=document.getElementById("screen1")
-let del=document.getElementById("del")
-let rest=document.getElementById("rest")
-let pre=document.getElementById("prefix")
-let post=document.getElementById("postfix")
-//************************************** */
+
+//*******boolans****** */
 let is_postfix=false
 let is_prefix=false
-
-//************************* */
+//***arrays */
 let L1=[]
 let s=[]
-
-
- 
-//*****functions******* */
-function screen_click(){
-    screen.textContent="input:"+L1.join(" ")
-}
-function appear_screan(a)
+//******dictionarys*******/
+const cont={plus:'+',minus:"-",times:"*",divide:"/",zero:"0",one:"1",two:"2",three:"3",four:"4",five:"5",six:"6",seven:"7",eight:"8",nine:"9"}
+const pcelements={}
+//****elements********** */
+for (const key in cont) 
 {
-L1.push(a)
-screen.textContent ="input:"+L1.join(" ")
-    
+    pcelements[key] = document.getElementById(cont[key]);
 }
-function appear_answer(value){
-    screen1.textContent="output:"+value
+const dark=document.getElementById("dark")
+const light=document.getElementById("light")
+const normal=document.getElementById("normal")
+const big_container=document.getElementById("big_container")
+const equal=document.getElementById("=")
+const screen_input=document.getElementById("screen")
+const screen_output=document.getElementById("screen1")
+const del=document.getElementById("del")
+const rest=document.getElementById("rest")
+const pre=document.getElementById("prefix")
+const post=document.getElementById("postfix")
+//*****functions******* */
+function add_to_screan(a)
+{
+    L1.push(a)
+    screen_input.textContent ="input: "+L1.join(" ")   
 }
-
-
-function rest_click(){
-    screen.textContent="input:"
-    screen1.textContent="output:"
+function appear_answer(value)
+{
+    screen_output.textContent="output:"+value
+}
+function rest_click()
+{
+    screen_input.textContent="input:"
+    screen_output.textContent="output:"
     post.style.backgroundColor="black"
     pre.style.backgroundColor="black"
     is_postfix=false
     is_prefix=false
     L1=[]
-    
 }
-function dele(){
+function dele()
+{
     L1.pop()
-   screen.textContent="input:"+ L1.join(" ")
-    
-
-
+    screen_input.textContent="input:"+ L1.join(" ")
 }
-function prefix_click(){
+function prefix_click()
+{
     is_prefix=true
     is_postfix=false
     pre.style.backgroundColor="green"
     post.style.backgroundColor="black"
-    
 }
-function postfix_click(){
+function postfix_click()
+{
     is_postfix=true
     is_prefix=false
     post.style.backgroundColor="green"
     pre.style.backgroundColor="black"
-    
 }
-function prefix(l){
-    if(!isNaN(l[0])){
+function prefix(l)
+{
+    if(!isNaN(l[0]))
+    {
         alert("definitly a mistake")    
        return ""
     }
 
     s=[]
-    for(let i=L1.length-1;i>=0;i--){
+    for(let i=L1.length-1;i>=0;i--)
+    {
         if(L1[i]=="+" ||L1[i]=="-" || L1[i]=="*"||L1[i]=="/")
-        {if (s.length<2){
-            alert("insufficient numbers ")
-            
-            return""
-        }
-        let v1=parseInt(s.pop());
-        let v2=parseInt(s.pop());
-        let result
-        if (L1[i]=="+")
-            result=v2+v1
-        else if(L1[i]=="-")
-            result=v1-v2
-        else if(L1[i]=="*")
-            result=v1*v2
-        else if(L1[i]=="/"){
-            if (v2==0){
-                alert("division by zero for opperation at index"+i)
-                return ""
-                
-              
+        {
+            if (s.length<2)
+            {
+                alert("insufficient numbers after operator of index "+i)
+                return""
+            }
+           let v1=parseInt(s.pop());
+           let v2=parseInt(s.pop());
+           let result
+           if (L1[i]=="+")
+              result=v2+v1
+           else if(L1[i]=="-")
+              result=v1-v2
+           else if(L1[i]=="*")
+              result=v1*v2
+           else if(L1[i]=="/")
+           {
+            if (v2==0)
+            {
+               alert("division by zero for opperation at index"+i)
+               return ""
             }
             result=v1/v2
-        }
-        s.push(result)
+           }
+           s.push(result)
         }
         else
             s.push(L1[i])
     }
     
-    if (s.length !== 1) {
+    if (s.length !== 1) 
+    {
        alert("not balance ")
        return ""
     }
-    
-    
     return s.pop()
-
 }
-function postfix(l){
-s=[]
-for(let i=0;i<L1.length;i++){
+function postfix(l)
+{
+    if(l.length==1)
+    {
+        alert("incomplete")    
+        return ""
+    }
+  s=[]
+  for(let i=0;i<L1.length;i++)
+   {
     if(L1[i]=="+"|| L1[i]=="-"||L1[i]=="*"||L1[i]=="/")
-    {   if (s.length<2){
-            alert("insufficient numbers")  
+    {   if (s.length<2)
+        {
+            alert("insufficient numbers before operatin of index"+i )  
             return  ""
         }
         let v2=parseInt(s.pop())
@@ -140,8 +137,10 @@ for(let i=0;i<L1.length;i++){
             result=v1-v2
         else if (L1[i]=="*")
             result=v1*v2
-        else if (L1[i]=="/"){
-            if(v2==0){
+        else if (L1[i]=="/")
+        {
+            if(v2==0)
+            {
                 alert("divide by zero by operation of index "+i+"you can add and del")
                 return ""
             }
@@ -150,11 +149,12 @@ for(let i=0;i<L1.length;i++){
            
         s.push(result)
     }
-    else{
+    else
         s.push(L1[i])
-    }
-}
-    if (s.length !== 1) {
+        
+   }
+    if (s.length !== 1) 
+    {
         alert(" not balance you can add and del ")
         return ""
     }
@@ -163,73 +163,48 @@ for(let i=0;i<L1.length;i++){
     
 
 }   
-function equal_click(){
-    if(L1.length===0){
+function equal_click()
+{
+    if(L1.length===0)
         alert("please enter numbers")
-    }
-    else if (is_postfix){
-        
+    else if (is_postfix)
+    {
         t=postfix(L1)
         appear_answer(t)
-
-        
     }
-    else if(is_prefix){
-        
+    else if(is_prefix)
+    {    
         t=prefix(L1)
        appear_answer(t)
-
     }
-    else{
+    else
         alert(" press on pre and post")
-
-    }
 }
-
-
-
-//************** */
-plus.addEventListener("click",function(){
-appear_screan("+")
-})
-minus.addEventListener("click",function(){
-    appear_screan("-")
-    })
-times.addEventListener("click",function(){
-    appear_screan("*")
-    })
-divide.addEventListener("click",function(){
-        appear_screan("/")
-        })
-zero.addEventListener("click",function(){
-            appear_screan("0")})   
-one.addEventListener("click",function(){
-            appear_screan("1")
-            })
-two.addEventListener("click",function(){
-                appear_screan("2")})
-three.addEventListener("click",function(){
-                    appear_screan("3")               
-})                           
-four.addEventListener("click",function(){
-    appear_screan("4")})
-five.addEventListener("click",function(){
-        appear_screan("5")})  
-six.addEventListener("click",function(){
-            appear_screan("6")
-}) 
-seven.addEventListener("click",function(){
-    appear_screan("7")
-})
-eight.addEventListener("click",function(){
-    appear_screan("8")
-}) 
-nine.addEventListener("click",function(){
-    appear_screan("9")
-}) 
+//******events ******** */
+for (const key in pcelements) 
+{
+    pcelements[key].addEventListener("click",
+        function () {
+        add_to_screan(cont[key]); 
+    });
+}
 del.addEventListener("click",dele)
 post.addEventListener("click",postfix_click)
 pre.addEventListener("click",prefix_click)
 rest.addEventListener("click",rest_click)
 equal.addEventListener("click",equal_click)
-screen.addEventListener("click",screen_click)
+dark.addEventListener("click",
+    function(){
+    big_container.style.backgroundColor="rgb(35, 29, 29)"
+    }
+)
+light.addEventListener("click",
+    function(){
+        big_container.style.backgroundColor="rgb(205, 195, 195)"
+    }
+)
+normal.addEventListener("click",
+    function(){
+        big_container.style.backgroundColor="rgb(118, 98, 98)"
+    }
+)
